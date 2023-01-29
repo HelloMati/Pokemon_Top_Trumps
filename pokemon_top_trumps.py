6cd7972228f00cdb1969c056f2a3c752e3452ee9
Mati <matimotof@gmail.com>
2023-01-29 21:30:21 +0000

    team updates

--- pokemon_top_trumps.py
+++ pokemon_top_trumps.py
@@ -1,5 +1,6 @@
 import random
 import requests
+
 def random_pokemon():
     pokemon_number = random.randint(1, 151)  #took a random number between 1 and 151
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_number}/"
@@ -10,15 +11,35 @@ def random_pokemon():
         'id': pokemon['id'],
         'height': pokemon['height'],
         'weight': pokemon['weight'],
+        'order': pokemon['order'],
     }
+
+def random_ability(): #takes a random ability for the pokemon
+    pokemon_ability = random.randint(1,151)
+    url = f"https://pokeapi.co/api/v2/ability/{pokemon_ability}/"
+    response = requests.get(url)
+    ability = response.json()
+    return{
+        #'id': ability['id'],
+        'name': ability['name'],
+    }
+
+
 def run(c=0,d=0): #added two parameters c and d as counters
+
     my_pokemon = random_pokemon()
     print(f"You were given {my_pokemon['name']}")
-    stat_choice = input('Which stat do you want to use? (id, height, weight) ') #player chooses the stat their pokemon would compete with
+    my_ability = random_ability()
+    print(f"Your pokemon has the {my_ability['name']} ability")
+    stat_choice = input('Which stat do you want to use? (id, height, weight, order) ') #player chooses the stat their pokemon would compete with
     opponent_pokemon = random_pokemon()
     print(f"The opponent chose {opponent_pokemon['name']}")
+    opponent_ability = random_ability()
+    print(f"Your opponent has {opponent_ability['name']} ability")
+
     my_stat = my_pokemon[stat_choice]
     opponent_stat = opponent_pokemon[stat_choice]
+
     if my_stat > opponent_stat:
         print('You Win!')
         c+=1
@@ -27,15 +48,17 @@ def run(c=0,d=0): #added two parameters c and d as counters
         d+=1
     else:
         print('Draw!')
-    if c>d: #verifies if the player won more rounds
+
+    if c > d:  # verifies if the player won more rounds
         print("You won more rounds. Congratulations!")
-    elif c<d: #verifies if the computer won more rounds
+    elif c < d:  # verifies if the computer won more rounds
         print("Your opponent won more rounds. Keeping training to be the very best.")
-    else: #verifies if c=d
+    else:  # verifies if c=d
         print("It is evenly matched.")
+
 while True:
-    run()
-    play_again = input("Would you like to play again? (y/n)")
-    if play_again.lower() == 'n':
-        break
-print("Thanks for playing! Come back to battle everyday with your pokemon team.") 
+        run()
+        play_again = input("Would you like to play again? (y/n)")
+        if play_again.lower() == 'n':
+            break
+print("Thanks for playing! Come back to battle everyday with your pokemon team.")
